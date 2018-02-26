c04693925b5d721ae8dde5fb1be7913e99234c24
Taylor Tram <taylor.tram@icloud.com>
2018-02-26 23:21:38 +0000

    Halfway there?

--- js/script.js
+++ js/script.js
@@ -6,16 +6,20 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  
-
+  $("#pig").click(function(){
+	var input = $("#input").val();
+    $("#place").append(piglat(input));
+  });
+     
 
 
 });
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
-
-
+function piglat (x){
+    return x + "ay";
+}
 
 
 
@@ -24,4 +28,11 @@ $( document ).ready(function() {
 	//Loops through all the words in the sentence and transforms each word
 	//It should return a transfromed sentance
 
-
+function sentencrToPigLatin (p){
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    if (p[0] in vowels) {
+        return sentenceToPigLatin.append
+    }
+    else {
+        
+    }
